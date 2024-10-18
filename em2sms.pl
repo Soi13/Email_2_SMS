527ae366caccbc56898cea44c79ff64f2705cb8c
Oleg Skvortsov <highsoft.soi@gmail.com>
2024-10-17 22:04:32 -0700

    Change functionality for extracting from email body only text, but not HTML data

--- em2sms.pl
+++ em2sms.pl
@@ -3,12 +3,15 @@ use warnings;
 
 use Mail::IMAPClient;
 use IO::Socket::SSL;
+use Email::MIME;
 
 #Let's define Gmail IMAP server
 my $imap_srv = "imap.gmail.com";
 my $port = 993;
 my $em = "";
 my $app_pswd = ""; #We need to generate an App Password since Google blocks apps from accessing your account with normal password.
+
+my $sender = "";
 
 #Create IMAP client
 my $imap_cl = Mail::IMAPClient->new(Server => $imap_srv, Port => $port, User => $em, Password => $app_pswd, Ssl => 1, Uid => 1) or die "Could not connect to IMAP server: $@";
@@ -17,16 +20,30 @@ my $imap_cl = Mail::IMAPClient->new(Server => $imap_srv, Port => $port, User => 
 $imap_cl->select("INBOX") or die "Could not select INBOX folder: ", $imap_cl->LastError;
 
 #Get unseen emails
-my @em_messages = $imap_cl->search("UNSEEN");
+my @em_messages = $imap_cl->search("FROM", $sender, "UNSEEN");
 
 foreach my $msg_id (@em_messages) {
+    my $raw_message = $imap_cl->message_string($msg_id);
+
+    my $email_mime = Email::MIME->new($raw_message);
+    # Get the text part of the email (ignoring HTML)
+    my $text_body = "";
+    for my $part ($email_mime->parts) {
+        if ($part->content_type =~ m{text/plain}i) {
+            $text_body = $part->body_str;
+            last;  # Exit the loop after finding the plain text part
+        }
+    }
+
     my $subject = $imap_cl->subject($msg_id);
     my $from = $imap_cl->get_header($msg_id, 'From');
-    my $body = $imap_cl->body_string($msg_id);
+    #my $body = $imap_cl->body_string($msg_id);
+    $imap_cl->deny_seeing($msg_id); #Keep messages in UNSEEN status after script read them
 
     print "From: $from\n";
     print "Subject: $subject\n";
-    print "Body: $body\n\n";
+    #print "Body: $body\n\n";
+    print $text_body;
 }
 
 $imap_cl->logout;
